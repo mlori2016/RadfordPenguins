6b7d14453c499eb9829afa24e0df847a7acee254
mlori2016 <mlori2016@users.noreply.github.com>
2016-10-16 21:32:55 -0400

    Add files via upload

---
+++ indextest.php
@@ -0,0 +1,41 @@
+<?php
+/***
+Author:  Lori Miller
+Index Page to use in Team Penguin Project
+
+
+***/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/transitional.dtd">
+<html xmlns="http://www.w3.org/1999/ehtml" xm:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<link href="companion.css" rel="stylesheet" type="text/css">
+
+	<title>Teacher's Companion</title>
+</head>
+<body>
+<?php
+//include nav bar
+include_once('template/navbar.php'); ?>
+	<div id="wrapper" >
+		<div id="content" >
+			<p>
+			<img src="images/lori.jpg" height="300" float="left" />
+			</p>
+			<p>
+				This is the Home Page where the user can view their profile, upload images to use as a profile photo and enter text that will show on thier profile. </p>
+					<div id="awards" ><br />
+						<h2>Awards Earned</h2><br />
+						<table>
+							<tr><img src="images/additionicon.png" width="80px"  /></tr>
+							<tr><img src="images/subtractionicon.png" width="80px"  /></tr>
+							<tr><img src="images/divisionicon.png" width="80px"  /></tr>
+							<tr><img src="images/multiplicationicon.png" width="80px"  /></tr>
+						</table>
+					<div>
+				</p>
+		</div>
+	</div>
+</body>
+</html>
